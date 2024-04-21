1a7589a2c9ad0727211b809dc24a515c4443c5e2
nandakatmenon <167697216+nandakatmenon@users.noreply.github.com>
2024-04-21 11:26:43 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,184 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, ActivityIndicator, SafeAreaView, ScrollView, FlatList, Alert, RefreshControl } from 'react-native';
+import * as Location from 'expo-location';
+
+const openWeatherKey = `849338767c0e95025b5559533d26b7c4`;
+let url = `https://api.openweathermap.org/data/2.5/onecall?&units=metric&exclude=minutely&appid=${openWeatherKey}`;
+
+
+const App = () => {
+
+  const [forecast, setForecast] = useState(null);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const loadForecast = async () => {
+    setRefreshing(true);
+
+    const { status } = await Location.requestPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert('Location access was denied');
+    }
+
+    let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+
+    const response = await fetch( `${url}&lat=${location.coords.latitude}&lon=${location.coords.longitude}`);
+    const data = await response.json();
+
+    if(!response.ok) {
+      Alert.alert(`There was an error in retrieving the weather data: ${data.message}`); 
+    } else {
+      setForecast(data);
+    }
+
+    setRefreshing(false);
+  }
+
+  useEffect(() => { 
+    if (!forecast) {
+      loadForecast(); 
+    }
+  })
+
+  if (!forecast) {
+    return <SafeAreaView style={styles.loading}>
+      <ActivityIndicator size="large" />
+      </SafeAreaView>;
+  }
+
+  const current = forecast.current.weather[0];
+ 
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView 
+        refreshControl={
+          <RefreshControl 
+            onRefresh={() => {  loadForecast() }} 
+            refreshing={refreshing}
+          />}
+      >
+        <Text style={styles.title}>BareWeather</Text>
+        <View style={styles.current}>
+          <Image
+            style={styles.largeIcon}
+            source={{
+              uri: `http://openweathermap.org/img/wn/${current.icon}@4x.png`,
+            }}
+          />
+          <Text style={styles.currentTemp}>{Math.round(forecast.current.temp)}°C</Text>
+        </View>
+        
+        <Text style={styles.currentDescription}>{current.description}</Text>
+        <View>
+          <Text style={styles.subtitle}>Hourly Forecast</Text>
+          <FlatList horizontal
+            data={forecast.hourly.slice(0, 24)}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={(hour) => {
+              const weather = hour.item.weather[0];
+              var dt = new Date(hour.item.dt * 1000);
+              return <View style={styles.hour}>
+                <Text>{dt.toLocaleTimeString().replace(/:\d+ /, ' ')}</Text>
+                <Text>{Math.round(hour.item.temp)}°C</Text>
+                <Image
+                  style={styles.smallIcon}
+                  source={{
+                    uri: `http://openweathermap.org/img/wn/${weather.icon}@4x.png`,
+                  }}
+                />
+                <Text>{weather.description}</Text>
+              </View>
+            }}
+          />
+        </View>
+
+        <Text style={styles.subtitle}>7-Day Forecast</Text>
+        {forecast.daily.slice(1,8).map(d => {
+          const weather = d.weather[0];
+          var dt = new Date(d.dt * 1000);
+          return <View style={styles.day} key={d.dt}>
+            <Text style={styles.dayTemp}>{Math.round(d.temp.max)}°C</Text>
+            <Image
+              style={styles.smallIcon}
+              source={{
+                uri: `http://openweathermap.org/img/wn/${weather.icon}@4x.png`,
+              }}
+            />
+            <View style={styles.dayDetails}>
+              <Text>{dt.toLocaleDateString()}</Text>
+              <Text>{weather.description}</Text>
+            </View>
+          </View>
+        })}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    width: '100%',
+    textAlign: 'center',
+    fontSize: 42,
+    color: '#e96e50',
+  },
+  subtitle: {
+    fontSize: 24,
+    marginVertical: 12,
+    marginLeft: 4,
+    color: '#e96e50',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#87CEEB',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: '#87CEEB',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  current: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  currentTemp: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },  
+  currentDescription: {
+    width: '100%',
+    textAlign: 'center',
+    fontWeight: '200',
+    fontSize: 24,
+    marginBottom: 24
+  },
+  hour: {
+    padding: 6,
+    alignItems: 'center',
+  },
+  day: {
+    flexDirection: 'row',
+  },
+  dayDetails: {
+    justifyContent: 'center',
+  },
+  dayTemp: {
+    marginLeft: 12,
+    alignSelf: 'center',
+    fontSize: 20
+  },
+  largeIcon: {
+    width: 250,
+    height: 200,
+  },
+  smallIcon: {
+    width: 100,
+    height: 100,
+  }
+});
+
+export default App;
